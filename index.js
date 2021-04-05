3cad5873b5e5b2b288d4891f1f85acd283ca6788
Alex Chivulescu <alex.chivulescu@dotts.ro>
2021-04-05 11:11:24 +0300

    refact register and login paths

--- index.js
+++ index.js
@@ -32,31 +32,46 @@ app.get("/", (req, res) => {
   res.send("123 123");
 });
 
-app.post("/auth", (req, res) => {
-  User.findOne({
-    email: req.body.email,
-  }).then((userFromDB) => {
-    bcrypt.compare(req.body.password, userFromDB.password, (err, result) => {
-      if (result) {
-        const accessToken = jwt.sign(
-          {
-            email: userFromDB.email,
-            firstName: userFromDB.firstName,
-            lastName: userFromDB.lastName,
-          },
-          process.env.JWT_SECRET
-        );
-
-        res.json({
-          accessToken,
-        });
-      }
-      if (err) return res.sendStatus(400);
+app.post("/auth", async (req, res) => {
+  let userFromDB;
+  let match;
+  try {
+    userFromDB = await User.findOne({
+      email: req.body.email,
     });
-  });
+
+    if (!userFromDB) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+  } catch (err) {
+    return res.status(400).json({ error: err });
+  }
+
+  try {
+    match = await bcrypt.compare(req.body.password, userFromDB.password);
+
+    if (match) {
+      const accessToken = jwt.sign(
+        {
+          email: userFromDB.email,
+          firstName: userFromDB.firstName,
+          lastName: userFromDB.lastName,
+        },
+        process.env.JWT_SECRET
+      );
+
+      res.json({
+        accessToken,
+      });
+    } else {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+  } catch (err) {
+    return res.status(400).json({ error: err });
+  }
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   if (
     !req.body.email ||
     !req.body.password ||
@@ -66,37 +81,35 @@ app.post("/register", (req, res) => {
     return res.status(400).json({ message: "All fields are mandatory." });
   }
 
-  User.findOne({ email: req.body.email })
-    .then((response) => {
-      if (response) {
-        return res
-          .status(400)
-          .json({ message: "This email is already in use." });
-      } else {
-        const newUser = new User({
-          email: req.body.email,
-          firstName: req.body.firstName,
-          lastName: req.body.lastName,
-          password: req.body.password,
-        });
+  let userFromDB;
+  let newUser;
+  try {
+    userFromDB = await User.findOne({ email: req.body.email });
+    if (userFromDB) {
+      return res.status(400).json({ message: "This email is already in use." });
+    } else {
+      newUser = new User({
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: req.body.password,
+      });
+    }
 
-        const salt = bcrypt.genSaltSync(saltRounds);
+    const salt = bcrypt.genSaltSync(saltRounds);
+    newUser.password = bcrypt.hashSync(newUser.password, salt);
 
-        newUser.password = bcrypt.hashSync(newUser.password, salt);
-        console.log(newUser);
-        newUser.save().then((savedUser) => {
-          return res.send({
-            email: savedUser.email,
-            firstName: savedUser.firstName,
-            lastName: savedUser.lastName,
-          });
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      return res.status(400).json({ message: err });
+    const savedUser = await newUser.save();
+
+    return res.send({
+      email: savedUser.email,
+      firstName: savedUser.firstName,
+      lastName: savedUser.lastName,
     });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ message: err });
+  }
 });
 
 app.listen(process.env.PORT, () => {
